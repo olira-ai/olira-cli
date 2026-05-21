5cf95c10f710069645446f195211b36bb80f95ab
Jorge Arias <jorge@raiahealth.com>
2026-05-21 10:46:47 +0100

    chore: bump to 1.0.4, add CODEOWNERS

--- homebrew/olira.rb
+++ homebrew/olira.rb
@@ -1,7 +1,7 @@
 class Olira < Formula
   desc "Olira AI - CLI for authenticating and configuring MCP access"
   homepage "https://olira.ai"
-  version "1.0.3"
+  version "1.0.4"
   license "MIT"
 
   on_macos do
